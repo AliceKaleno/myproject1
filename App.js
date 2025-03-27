d0996cf6fc71a8cd5903af1cc708b95349dae0b1
Alice_Silva <159441822+AliceKaleno@users.noreply.github.com>
2025-03-27 11:04:51 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Alice Maria da silva</Text>
+      <Text>Millena clara Cavalcanti</Text>
       <StatusBar style="auto" />
     </View>
   );
